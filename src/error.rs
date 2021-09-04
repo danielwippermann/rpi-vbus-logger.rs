cb32ba8c8681c872c97486ffeaa7d33fa00b5f14
Daniel Wippermann <daniel.wippermann@gmail.com>
2021-09-04 16:52:51 +0200

    Simplify `Error` module.

--- src/error.rs
+++ src/error.rs
@@ -1,46 +1,20 @@
 #[derive(Debug)]
-pub enum Error {
-    AppError(String),
-    IoError(::std::io::Error),
-    MysqlError(::mysql::Error),
-    SerialError(::serial::Error),
-    TomlDeError(::toml::de::Error),
-}
+pub struct Error(String);
 
+pub trait IntoError: std::fmt::Debug {}
 
-impl From<String> for Error {
-    fn from(inner: String) -> Error {
-        Error::AppError(inner)
+impl <T: IntoError> From<T> for Error {
+    fn from(other: T) -> Error {
+        Error(format!("{}: {:?}", std::any::type_name::<T>(), other))
     }
 }
 
-
-impl From<::std::io::Error> for Error {
-    fn from(inner: ::std::io::Error) -> Error {
-        Error::IoError(inner)
-    }
-}
-
-
-impl From<::mysql::Error> for Error {
-    fn from(inner: ::mysql::Error) -> Error {
-        Error::MysqlError(inner)
-    }
-}
-
-
-impl From<::serial::Error> for Error {
-    fn from(inner: ::serial::Error) -> Error {
-        Error::SerialError(inner)
-    }
-}
-
-
-impl From<::toml::de::Error> for Error {
-    fn from(inner: ::toml::de::Error) -> Error {
-        Error::TomlDeError(inner)
-    }
-}
-
+impl IntoError for &str {}
+impl IntoError for String {}
+impl IntoError for std::io::Error {}
+impl IntoError for mysql::Error {}
+impl IntoError for resol_vbus::Error {}
+impl IntoError for serial::Error {}
+impl IntoError for toml::de::Error {}
 
 pub type Result<T> = ::std::result::Result<T, Error>;
