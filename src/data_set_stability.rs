bfbf3aa5a84f5c19b50187f3603a875b6a4be2ae
Daniel Wippermann <daniel.wippermann@gmail.com>
2021-09-04 16:52:40 +0200

    Improve `DataSetStability` module.

--- src/data_set_stability.rs
+++ src/data_set_stability.rs
@@ -1,5 +1,11 @@
-use resol_vbus::DataSet;
+use resol_vbus::{Data, DataSet};
 
+pub enum DataSetStabilityState {
+    DataSetChanged,
+    Stabilizing(i32),
+    Stabilized,
+    Stable,
+}
 
 pub struct DataSetStability {
     data_set: DataSet,
@@ -7,15 +13,21 @@ pub struct DataSetStability {
     is_stable: bool,
 }
 
-
 impl DataSetStability {
-
     pub fn new() -> DataSetStability {
         DataSetStability {
             data_set: DataSet::new(),
             counter: 0,
             is_stable: false,
         }
+    }
+
+    pub fn data_set(&self) -> &DataSet {
+        &self.data_set
+    }
+
+    pub fn as_data_slice(&self) -> &[Data] {
+        self.data_set.as_data_slice()
     }
 
     pub fn is_stable(&self) -> bool {
@@ -33,19 +45,37 @@ impl DataSetStability {
         }
     }
 
-    pub fn add_data_set(&mut self, other: DataSet) {
+    pub fn add_data(&mut self, other: Data) -> DataSetStabilityState {
         let old_len = self.data_set.len();
 
-        self.data_set.add_data_set(other);
+        self.data_set.add_data(other);
 
+        self.handle_added_data(old_len)
+    }
+
+    // pub fn add_data_set(&mut self, other: DataSet) -> DataSetStabilityState {
+    //     let old_len = self.data_set.len();
+
+    //     self.data_set.add_data_set(other);
+
+    //     self.handle_added_data(old_len)
+    // }
+
+    fn handle_added_data(&mut self, old_len: usize) -> DataSetStabilityState {
         if self.data_set.len() != old_len {
             self.counter = 0;
             self.is_stable = false;
+            DataSetStabilityState::DataSetChanged
         } else if self.is_stable {
-            // nop
-        } else if self.stability_percent() >= 100 {
-            self.is_stable = true;
+            DataSetStabilityState::Stable
+        } else {
+            let percent = self.stability_percent();
+            if percent >= 100 {
+                self.is_stable = true;
+                DataSetStabilityState::Stabilized
+            } else {
+                DataSetStabilityState::Stabilizing(percent)
+            }
         }
     }
-
 }
